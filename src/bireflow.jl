a8b41c05c11622e8a5319386a5ff52b6133ee8ec
Rui Zhou <rui.zhou@student.unimelb.edu.au>
2025-11-25 10:34:47 +1100

    Real birefringence operator

--- src/bireflow.jl
+++ src/bireflow.jl
@@ -1,27 +1,28 @@
-struct BireFlow
-    α::Any
+abstract type BireFlowOp{T} <: FlowOpWithAdjoint{T} end
+
+struct BireFlow{T} <: BireFlowOp{T}
+    α::Field
 end
 
-function (G::BireFlow)(f)
+BireFlow(α::Field) = BireFlow{real(eltype(α))}(α)
 
-    E_before = f.E
-    B_before = f.B
-
+function (G::BireFlow)(f::Field)
     α = G.α
 
-    E_new = cos(2 * α) .* E_before .- sin(2 * α) .* B_before
-    B_new = sin(2 * α) .* E_before .+ cos(2 * α) .* B_before
+    f_iqu = IQUFourier(f)
 
-    # return E_before, B_before, E_after, B_after
+    Q = f_iqu.Q
+    U = f_iqu.U
 
-    f_new = copy(f)      # same type, metadata, storage, projection
+    Q_new = cos.(2 .* α) .* Q .- sin.(2 .* α) .* U
+    U_new = sin.(2 .* α) .* Q .+ cos.(2 .* α) .* U
 
-    # f.arr has shape (Ny/2+1, Nx, 2 [, batch])
-    f_new.arr[:, :, 1] .= E_new.arr
-    f_new.arr[:, :, 2] .= B_new.arr
+    f_iqu_new = copy(f_iqu)
+    f_iqu_new.arr[:, :, end-1] .= Q_new.arr
+    f_iqu_new.arr[:, :, end]   .= U_new.arr
 
-    return f_new
+    return IEBFourier(f_iqu_new)
 end
 
 import Base: *
-*(G::BireFlow, f) = G(f)
+*(G::BireFlow, f::Field) = G(f)
