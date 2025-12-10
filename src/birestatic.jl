2bf7202e0feb3f793a0672cce7d7fd08c23ee829
Rui Zhou <rui.zhou@student.unimelb.edu.au>
2025-12-11 10:44:40 +1100

    Correct joint estimation of lensing and birefringence.

--- src/birestatic.jl
+++ src/birestatic.jl
@@ -1,3 +1,295 @@
+# abstract type BireStaticOp{T} <: StaticOpWithAdjoint{T} end
+
+# struct BireStatic{T} <: BireStaticOp{T}
+#     α::Field
+# end
+
+# BireStatic(α::Field) = BireStatic{real(eltype(α))}(α)
+
+# function (R::BireStatic)(f::Field)
+#     α = Map(R.α)
+#     f_iqu = IQUMap(f)
+
+#     Q = f_iqu.Q
+#     U = f_iqu.U
+
+#     Q_rot =  cos.(2 .* α) .* Q .- sin.(2 .* α) .* U
+#     U_rot =  sin.(2 .* α) .* Q .+ cos.(2 .* α) .* U
+
+#     f_iqu_rot = copy(f_iqu)
+#     f_iqu_rot.arr[:, :, end-1] .= Q_rot.arr
+#     f_iqu_rot.arr[:, :, end]   .= U_rot.arr
+
+#     return IQUFourier(f_iqu_rot)
+# end
+
+# # forward, adjoint, inverse and inverse-adjoint
+# import Base: *, adjoint, /
+
+# *(R::BireStatic, f::Field) = R(f)
+# *(Radj::Adjoint{<:Any,<:BireStatic}, f::Field) = BireStatic(-Radj.parent.α)(f)
+# \(R::BireStatic, f::Field) = BireStatic(-R.α)(f)
+# \(Radj::Adjoint{<:Any,<:BireStatic}, f::Field) = BireStatic(Radj.parent.α)(f)
+
+# import ChainRulesCore: rrule, NoTangent, ZeroTangent, AbstractTangent
+
+# # function rrule(::typeof(*), R::BireStatic, f::Field)
+# #     y = R * f
+# #     pullback(Δ) = (NoTangent(), ZeroTangent(), ZeroTangent())
+# #     return y, pullback
+# # end
+
+# # function rrule(::typeof(*), Radj::Adjoint{<:Any,<:BireStatic}, f::Field)
+# #     y = Radj * f
+# #     pullback(Δ) = (NoTangent(), ZeroTangent(), ZeroTangent())
+# #     return y, pullback
+# # end
+
+# # function rrule(::typeof(\), R::BireStatic, f::Field)
+# #     y = R \ f
+# #     pullback(Δ) = (NoTangent(), ZeroTangent(), ZeroTangent())
+# #     return y, pullback
+# # end
+
+# # function rrule(::typeof(\), Radj::Adjoint{<:Any,<:BireStatic}, f::Field)
+# #     y = Radj \ f
+# #     pullback(Δ) = (NoTangent(), ZeroTangent(), ZeroTangent())
+# #     return y, pullback
+# # end
+
+# function rrule(::typeof(*), R::BireStatic, f::Field)
+#     α = Map(R.α)
+#     f_iqu = IQUMap(f)
+#     Q = f_iqu.Q
+#     U = f_iqu.U
+    
+#     cos_2α = cos.(2 .* α)
+#     sin_2α = sin.(2 .* α)
+    
+#     Q_rot = cos_2α .* Q .- sin_2α .* U
+#     U_rot = sin_2α .* Q .+ cos_2α .* U
+    
+#     f_iqu_rot = copy(f_iqu)
+#     f_iqu_rot.arr[:, :, end-1] .= Q_rot.arr
+#     f_iqu_rot.arr[:, :, end] .= U_rot.arr
+#     y = IQUFourier(f_iqu_rot)
+    
+#     function pullback(Δ)
+#         Δ_iqu = IQUMap(Δ)
+#         ΔQ = Δ_iqu.Q
+#         ΔU = Δ_iqu.U
+        
+#         Q_back = cos_2α .* ΔQ .+ sin_2α .* ΔU
+#         U_back = -sin_2α .* ΔQ .+ cos_2α .* ΔU
+        
+#         f_iqu_back = copy(f_iqu)
+#         f_iqu_back.arr[:, :, end-1] .= Q_back.arr
+#         f_iqu_back.arr[:, :, end] .= U_back.arr
+#         ∂f = IQUFourier(f_iqu_back)
+        
+#         ∂α_from_Q = -2 .* sin_2α .* Q .- 2 .* cos_2α .* U
+#         ∂α_from_U = 2 .* cos_2α .* Q .- 2 .* sin_2α .* U
+        
+#         ∂α = similar(α)
+#         ∂α.arr .= ∂α_from_Q.arr .* ΔQ.arr .+ ∂α_from_U.arr .* ΔU.arr
+        
+#         return (NoTangent(), (α = ∂α,), ∂f)
+#     end
+    
+#     return y, pullback
+# end
+
+# function rrule(::typeof(*), Radj::Adjoint{<:Any,<:BireStatic}, f::Field)
+#     R = Radj.parent
+#     α = Map(R.α)
+#     f_iqu = IQUMap(f)
+#     Q = f_iqu.Q
+#     U = f_iqu.U
+    
+#     cos_2α = cos.(2 .* α)
+#     sin_2α = sin.(2 .* α)
+    
+#     Q_rot = cos_2α .* Q .+ sin_2α .* U
+#     U_rot = -sin_2α .* Q .+ cos_2α .* U
+    
+#     f_iqu_rot = copy(f_iqu)
+#     f_iqu_rot.arr[:, :, end-1] .= Q_rot.arr
+#     f_iqu_rot.arr[:, :, end] .= U_rot.arr
+#     y = IQUFourier(f_iqu_rot)
+    
+#     function pullback(Δ)
+#         Δ_iqu = IQUMap(Δ)
+#         ΔQ = Δ_iqu.Q
+#         ΔU = Δ_iqu.U
+        
+#         Q_back = cos_2α .* ΔQ .- sin_2α .* ΔU
+#         U_back = sin_2α .* ΔQ .+ cos_2α .* ΔU
+        
+#         f_iqu_back = copy(f_iqu)
+#         f_iqu_back.arr[:, :, end-1] .= Q_back.arr
+#         f_iqu_back.arr[:, :, end] .= U_back.arr
+#         ∂f = IQUFourier(f_iqu_back)
+        
+#         ∂α_from_Q = 2 .* sin_2α .* Q .- 2 .* cos_2α .* U
+#         ∂α_from_U = 2 .* cos_2α .* Q .+ 2 .* sin_2α .* U
+        
+#         ∂α = similar(α)
+#         ∂α.arr .= ∂α_from_Q.arr .* ΔQ.arr .+ ∂α_from_U.arr .* ΔU.arr
+        
+#         return (NoTangent(), (parent = (α = ∂α,),), ∂f)
+#     end
+    
+#     return y, pullback
+# end
+
+# function rrule(::typeof(\), R::BireStatic, f::Field)
+#     α = Map(R.α)
+#     f_iqu = IQUMap(f)
+#     Q = f_iqu.Q
+#     U = f_iqu.U
+    
+#     cos_2α = cos.(2 .* α)
+#     sin_2α = sin.(2 .* α)
+    
+#     Q_rot = cos_2α .* Q .+ sin_2α .* U
+#     U_rot = -sin_2α .* Q .+ cos_2α .* U
+    
+#     f_iqu_rot = copy(f_iqu)
+#     f_iqu_rot.arr[:, :, end-1] .= Q_rot.arr
+#     f_iqu_rot.arr[:, :, end] .= U_rot.arr
+#     y = IQUFourier(f_iqu_rot)
+    
+#     function pullback(Δ)
+#         Δ_iqu = IQUMap(Δ)
+#         ΔQ = Δ_iqu.Q
+#         ΔU = Δ_iqu.U
+        
+#         Q_back = cos_2α .* ΔQ .- sin_2α .* ΔU
+#         U_back = sin_2α .* ΔQ .+ cos_2α .* ΔU
+        
+#         f_iqu_back = copy(f_iqu)
+#         f_iqu_back.arr[:, :, end-1] .= Q_back.arr
+#         f_iqu_back.arr[:, :, end] .= U_back.arr
+#         ∂f = IQUFourier(f_iqu_back)
+        
+#         ∂α_from_Q = -2 .* sin_2α .* Q .+ 2 .* cos_2α .* U
+#         ∂α_from_U = -2 .* cos_2α .* Q .- 2 .* sin_2α .* U
+        
+#         ∂α = similar(α)
+#         ∂α.arr .= ∂α_from_Q.arr .* ΔQ.arr .+ ∂α_from_U.arr .* ΔU.arr
+        
+#         return (NoTangent(), (α = ∂α,), ∂f)
+#     end
+    
+#     return y, pullback
+# end
+
+# function rrule(::typeof(\), Radj::Adjoint{<:Any,<:BireStatic}, f::Field)
+#     R = Radj.parent
+#     α = Map(R.α)
+#     f_iqu = IQUMap(f)
+#     Q = f_iqu.Q
+#     U = f_iqu.U
+    
+#     cos_2α = cos.(2 .* α)
+#     sin_2α = sin.(2 .* α)
+    
+#     Q_rot = cos_2α .* Q .- sin_2α .* U
+#     U_rot = sin_2α .* Q .+ cos_2α .* U
+    
+#     f_iqu_rot = copy(f_iqu)
+#     f_iqu_rot.arr[:, :, end-1] .= Q_rot.arr
+#     f_iqu_rot.arr[:, :, end] .= U_rot.arr
+#     y = IQUFourier(f_iqu_rot)
+    
+#     function pullback(Δ)
+#         Δ_iqu = IQUMap(Δ)
+#         ΔQ = Δ_iqu.Q
+#         ΔU = Δ_iqu.U
+        
+#         Q_back = cos_2α .* ΔQ .+ sin_2α .* ΔU
+#         U_back = -sin_2α .* ΔQ .+ cos_2α .* ΔU
+        
+#         f_iqu_back = copy(f_iqu)
+#         f_iqu_back.arr[:, :, end-1] .= Q_back.arr
+#         f_iqu_back.arr[:, :, end] .= U_back.arr
+#         ∂f = IQUFourier(f_iqu_back)
+        
+#         ∂α_from_Q = -2 .* sin_2α .* Q .- 2 .* cos_2α .* U
+#         ∂α_from_U = 2 .* cos_2α .* Q .- 2 .* sin_2α .* U
+        
+#         ∂α = similar(α)
+#         ∂α.arr .= ∂α_from_Q.arr .* ΔQ.arr .+ ∂α_from_U.arr .* ΔU.arr
+        
+#         return (NoTangent(), (parent = (α = ∂α,),), ∂f)
+#     end
+    
+#     return y, pullback
+# end
+
+# # function rrule(::Type{<:BireStatic}, α::Field)
+# #     y = BireStatic(α)
+# #     pullback(Δ) = (NoTangent(), ZeroTangent())
+# #     return y, pullback
+# # end
+
+# # function rrule(::Type{<:BireStatic{T}}, α::Field) where T
+# #     y = BireStatic{T}(α)
+# #     pullback(Δ) = (NoTangent(), ZeroTangent())
+# #     return y, pullback
+# # end
+
+# # function rrule(::Type{<:BireStatic}, α::Field)
+# #     y = BireStatic(α)
+# #     pullback(Δ) = (NoTangent(), Δ.α)
+# #     return y, pullback
+# # end
+
+# # function rrule(::Type{<:BireStatic{T}}, α::Field) where T
+# #     y = BireStatic{T}(α)
+# #     pullback(Δ) = (NoTangent(), Δ.α)
+# #     return y, pullback
+# # end
+
+# using ChainRulesCore: unthunk
+
+# function rrule(::Type{<:BireStatic}, α::Field)
+#     y = BireStatic(α)
+#     function pullback(Δ)
+#         Δ_unwrapped = unthunk(Δ)
+#         ∂α = hasproperty(Δ_unwrapped, :α) ? Δ_unwrapped.α : ZeroTangent()
+#         return (NoTangent(), ∂α)
+#     end
+#     return y, pullback
+# end
+
+# function rrule(::Type{<:BireStatic{T}}, α::Field) where T
+#     y = BireStatic{T}(α)
+#     function pullback(Δ)
+#         Δ_unwrapped = unthunk(Δ)
+#         ∂α = hasproperty(Δ_unwrapped, :α) ? Δ_unwrapped.α : ZeroTangent()
+#         return (NoTangent(), ∂α)
+#     end
+#     return y, pullback
+# end
+
+# *(R::BireStatic, t::AbstractTangent) = ZeroTangent()
+# *(Radj::Adjoint{<:Any,<:BireStatic}, t::AbstractTangent) = ZeroTangent()
+# \(R::BireStatic, t::AbstractTangent) = ZeroTangent()
+# \(Radj::Adjoint{<:Any,<:BireStatic}, t::AbstractTangent) = ZeroTangent()
+
+# import ChainRulesCore: AbstractTangent
+
+# *(t::AbstractTangent, ::Any) = ZeroTangent()
+# *(::Any, t::AbstractTangent) = ZeroTangent()
+
+# for T in [BireStatic, Adjoint{<:Any,<:BireStatic}]
+#     @eval begin
+#         *(t::AbstractTangent, ::$T) = ZeroTangent()
+#         *( ::$T, t::AbstractTangent) = ZeroTangent()
+#     end
+# end
+
 abstract type BireStaticOp{T} <: StaticOpWithAdjoint{T} end
 
 struct BireStatic{T} <: BireStaticOp{T}
@@ -6,7 +298,6 @@ end
 
 BireStatic(α::Field) = BireStatic{real(eltype(α))}(α)
 
-# Forward operator R(α)*f
 function (R::BireStatic)(f::Field)
     α = Map(R.α)
     f_iqu = IQUMap(f)
@@ -24,54 +315,132 @@ function (R::BireStatic)(f::Field)
     return IQUFourier(f_iqu_rot)
 end
 
-import Base: *
+# forward, adjoint, inverse and inverse-adjoint
+import Base: *, adjoint, /
+
 *(R::BireStatic, f::Field) = R(f)
+*(Radj::Adjoint{<:Any,<:BireStatic}, f::Field) = BireStatic(-Radj.parent.α)(f)
+\(R::BireStatic, f::Field) = BireStatic(-R.α)(f)
+\(Radj::Adjoint{<:Any,<:BireStatic}, f::Field) = BireStatic(Radj.parent.α)(f)
 
-# Adjoint operator R' = R(-α)
-import Base: adjoint
-*(Radj::Adjoint{<:Any,<:BireStatic}, f::Field) =
-    BireStatic(-Radj.parent.α)(f)
+import ChainRulesCore: rrule, NoTangent, ZeroTangent, AbstractTangent, Tangent, unthunk
 
-# Inverse operator R \ f = R(-α)*f
-import Base: \
-\(R::BireStatic, f::Field) =
-    BireStatic(-R.α)(f)
+# --- ChainRulesCore rrule definitions ---
 
-# Inverse-adjoint operator R' \ f = R(+α)*f
-\(Radj::Adjoint{<:Any,<:BireStatic}, f::Field) =
-    BireStatic(Radj.parent.α)(f)
-
-# AD rule for R*f: no gradients, treat α as constant
-import ChainRulesCore: rrule, NoTangent, ZeroTangent, AbstractTangent
 function rrule(::typeof(*), R::BireStatic, f::Field)
-    y = R * f
-    pullback(Δ) = (NoTangent(), ZeroTangent(), ZeroTangent())
+    α = Map(R.α)
+    f_iqu = IQUMap(f)
+    Q = f_iqu.Q
+    U = f_iqu.U
+    
+    cos_2α = cos.(2 .* α)
+    sin_2α = sin.(2 .* α)
+    
+    Q_rot = cos_2α .* Q .- sin_2α .* U
+    U_rot = sin_2α .* Q .+ cos_2α .* U
+    
+    f_iqu_rot = copy(f_iqu)
+    f_iqu_rot.arr[:, :, end-1] .= Q_rot.arr
+    f_iqu_rot.arr[:, :, end] .= U_rot.arr
+    y = IQUFourier(f_iqu_rot)
+    
+    function pullback(Δ)
+        Δ_iqu = IQUMap(Δ)
+        ΔQ = Δ_iqu.Q
+        ΔU = Δ_iqu.U
+        
+        Q_back = cos_2α .* ΔQ .+ sin_2α .* ΔU
+        U_back = -sin_2α .* ΔQ .+ cos_2α .* ΔU
+        
+        f_iqu_back = copy(f_iqu)
+        f_iqu_back.arr[:, :, end-1] .= Q_back.arr
+        f_iqu_back.arr[:, :, end] .= U_back.arr
+        ∂f = IQUFourier(f_iqu_back)
+        
+        ∂α_from_Q = -2 .* sin_2α .* Q .- 2 .* cos_2α .* U
+        ∂α_from_U = 2 .* cos_2α .* Q .- 2 .* sin_2α .* U
+        
+        ∂α = similar(α)
+        ∂α.arr .= ∂α_from_Q.arr .* ΔQ.arr .+ ∂α_from_U.arr .* ΔU.arr
+        
+        ∂R = Tangent{typeof(R)}(α = ∂α)
+        return (NoTangent(), ∂R, ∂f)
+    end
+    
     return y, pullback
 end
 
-# AD rule for R'*f
 function rrule(::typeof(*), Radj::Adjoint{<:Any,<:BireStatic}, f::Field)
-    y = Radj * f
-    pullback(Δ) = (NoTangent(), ZeroTangent(), ZeroTangent())
+    R = Radj.parent
+    α = Map(R.α)
+    f_iqu = IQUMap(f)
+    Q = f_iqu.Q
+    U = f_iqu.U
+    
+    cos_2α = cos.(2 .* α)
+    sin_2α = sin.(2 .* α)
+    
+    Q_rot = cos_2α .* Q .+ sin_2α .* U
+    U_rot = -sin_2α .* Q .+ cos_2α .* U
+    
+    f_iqu_rot = copy(f_iqu)
+    f_iqu_rot.arr[:, :, end-1] .= Q_rot.arr
+    f_iqu_rot.arr[:, :, end] .= U_rot.arr
+    y = IQUFourier(f_iqu_rot)
+    
+    function pullback(Δ)
+        Δ_iqu = IQUMap(Δ)
+        ΔQ = Δ_iqu.Q
+        ΔU = Δ_iqu.U
+        
+        Q_back = cos_2α .* ΔQ .- sin_2α .* ΔU
+        U_back = sin_2α .* ΔQ .+ cos_2α .* ΔU
+        
+        f_iqu_back = copy(f_iqu)
+        f_iqu_back.arr[:, :, end-1] .= Q_back.arr
+        f_iqu_back.arr[:, :, end] .= U_back.arr
+        ∂f = IQUFourier(f_iqu_back)
+        
+        ∂α_from_Q = 2 .* sin_2α .* Q .- 2 .* cos_2α .* U
+        ∂α_from_U = 2 .* cos_2α .* Q .+ 2 .* sin_2α .* U
+        
+        ∂α = similar(α)
+        ∂α.arr .= ∂α_from_Q.arr .* ΔQ.arr .+ ∂α_from_U.arr .* ΔU.arr
+        
+        ∂R = Tangent{typeof(R)}(α = ∂α)
+        return (NoTangent(), ∂R', ∂f)
+    end
+    
     return y, pullback
 end
 
-# AD rule for R\f
-function rrule(::typeof(\), R::BireStatic, f::Field)
-    y = R \ f
-    pullback(Δ) = (NoTangent(), ZeroTangent(), ZeroTangent())
+function rrule(::Type{<:BireStatic}, α::Field)
+    y = BireStatic(α)
+    function pullback(Δ)
+        Δ_unwrapped = unthunk(Δ)
+        ∂α = hasproperty(Δ_unwrapped, :α) ? Δ_unwrapped.α : ZeroTangent()
+        return (NoTangent(), ∂α)
+    end
     return y, pullback
 end
 
-# AD rule for R'\f
-function rrule(::typeof(\), Radj::Adjoint{<:Any,<:BireStatic}, f::Field)
-    y = Radj \ f
-    pullback(Δ) = (NoTangent(), ZeroTangent(), ZeroTangent())
+function rrule(::Type{<:BireStatic{T}}, α::Field) where T
+    y = BireStatic{T}(α)
+    function pullback(Δ)
+        Δ_unwrapped = unthunk(Δ)
+        ∂α = hasproperty(Δ_unwrapped, :α) ? Δ_unwrapped.α : ZeroTangent()
+        return (NoTangent(), ∂α)
+    end
     return y, pullback
 end
 
-# Kill any tangent inputs: R*t → 0
 *(R::BireStatic, t::AbstractTangent) = ZeroTangent()
 *(Radj::Adjoint{<:Any,<:BireStatic}, t::AbstractTangent) = ZeroTangent()
-\(R::BireStatic, t::AbstractTangent) = ZeroTangent()
-\(Radj::Adjoint{<:Any,<:BireStatic}, t::AbstractTangent) = ZeroTangent()
+*(t::AbstractTangent, R::BireStatic) = ZeroTangent()
+*(t::AbstractTangent, Radj::Adjoint{<:Any,<:BireStatic}) = ZeroTangent()
+
+import ChainRulesCore: AbstractTangent, ZeroTangent
+
+function Base.:*(t::AbstractTangent, Radj::Adjoint{<:Any, <:CachedLenseFlow})
+    return ZeroTangent()
+end
